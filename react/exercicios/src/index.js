ff3de02cf3217bc1ee617f4c73f05653b4b75790
ceschini <ceschini@protonmail.com>
2020-04-22 22:44:08 -0300

    projetos react finalizado

---
+++ react/exercicios/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// componente personalizado sempre comeca com letra maiuscula
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
+
+ReactDOM.render(
+    <div>
+        <Pai nome="Paulo" sobrenome="Silva">
+            <Filho nome="Pedro" sobrenome="Silva" />
+            <Filho nome="Paulo" sobrenome="Silva" />
+            <Filho nome="Carla" sobrenome="Silva" />
+        </Pai>
+    </div>
+    , document.getElementById('root'))
